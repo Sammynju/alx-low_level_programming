dbfca8f0cfaca2817920be4e48d5dc2781fb1c10
Sammynju <sammynju2@gmail.com>
2022-09-08 09:13:41 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 after printing the function
+ *
+ *     */
+
+int main(void)
+
+{puts("\"programming is like building multilingual puzzle");
+
+	    return (0);
+
+}
