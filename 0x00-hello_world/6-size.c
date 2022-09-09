d2353ad08ffabb7565479a8123986a5723606a2a
Sammynju <sammynju2@gmail.com>
2022-09-08 20:00:47 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+return (0);
+}
